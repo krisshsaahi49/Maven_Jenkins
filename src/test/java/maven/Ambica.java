0f7d0c3932e2151434bd6998f9eb05de90587bdb
krisshsaahi49 <krisshsaahi@outlook.com>
2019-12-20 09:54:28 +0530

    Initial commit

---
+++ src/test/java/maven/Ambica.java
@@ -0,0 +1,11 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class Ambica {
+
+	@Test
+	public void a() {
+		System.out.println("Dabbaa Ambikaaaaa!!!!!!!");
+	}
+}
---
+++ src/test/java/maven/Usha.java
@@ -0,0 +1,13 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class Usha {
+
+	@Test
+	public void  main() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello dabba Ushaa!!!!!!!");
+	}
+
+}
